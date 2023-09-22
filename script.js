24b5fde71f0e564ae56f907d5aa8971bfe70b266
travisdbarzee <134562417+travisdbarzee@users.noreply.github.com>
2023-09-22 13:28:31 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 function navDropdown() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
- var y = document.getElementById('dropdown-btn')
+var y = document.getElementById("dropdown-btn");
 
- y.addEventListener("toggle", revealMore)
+y.addEventListener("toggle", revealMore);
 
- function revealMore() {
-y.classList.toggle('visible')
- }
+function revealMore() {
+  y.classList.toggle("visible");
+}
 
-  function scrollFunction() {
-    let e = document.getElementById("audition-section");
-    e.scrollIntoView({
-      block: 'start',
-      behavior: 'smooth',
-      inline: 'start'
-    });
-  }
+function scrollFunction() {
+  let e = document.getElementById("audition-section");
+  e.scrollIntoView({
+    block: "start",
+    behavior: "smooth",
+    inline: "start",
+  });
+}
 
-  const form = document.getElementById("myForm")
+const form = document.getElementById("myForm");
 
-  function openForm() {
-    form.style.display = "block"
-  }
-  
-  function closeForm() {
-    form.style.display = "none";
-  }
+function openForm() {
+  form.style.display = "block";
+}
 
-  function formSubmitted() {
-    alert("Thank you!")
-  }
+function closeForm() {
+  form.style.display = "none";
+}
+
+function formSubmitted() {
+  alert("Form submitted successfully!");
+}
