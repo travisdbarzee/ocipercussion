8984f3a316acac293a935597e40e8ed08143b75a
travisdbarzee <134562417+travisdbarzee@users.noreply.github.com>
2023-09-27 16:20:39 -0500

    Add files via upload

---
+++ staff-bios/bios.js
@@ -0,0 +1,16 @@
+function navDropdown() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var y = document.getElementById("dropdown-btn");
+
+y.addEventListener("toggle", revealMore);
+
+function revealMore() {
+  y.classList.toggle("visible");
+}
